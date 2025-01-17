93917810d367dccc7c4d9457874aae03589b215a
Edzer Dionido <jeddionido26@gmail.com>
2025-01-17 10:38:03 +0800

    add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router";
+
+import AppLayout from "../src/ui/AppLayout";
+import Dashboard from "./budget-tracker/Dashboard";
 
 function App() {
-
   return (
-    <div>
-      Hello World
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route element={<AppLayout />}>
+          <Route index element={<Navigate replace to="dashboard" />} />
+          <Route path="dashboard" element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
