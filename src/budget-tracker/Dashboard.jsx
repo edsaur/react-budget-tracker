3aea1c25f11d9140ed4b286785e6753eec25450d
Edzer Dionido <jeddionido26@gmail.com>
2025-01-17 10:36:29 +0800

    add dashboard

---
+++ src/budget-tracker/Dashboard.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+const StyledDashboard = styled.div`
+
+`;
+
+export default function Dashboard() {
+    return (
+        <StyledDashboard>
+            hello world
+        </StyledDashboard>
+    )
+}
